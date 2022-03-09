e761661e69cdb3448c8c55897f5bb265778f4254
Tennyson Ben-kalio <officialtennysonbk@gmail.com>
2022-03-09 15:20:16 +0100

    fetching data from Firebase

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,6 +1,24 @@
+import { useNavigate } from 'react-router-dom';
+
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupPage() {
+    const history = useNavigate();
+
+    function addMeetupHander(meetupData) {
+        fetch(
+            'https://react-checker-default-rtdb.firebaseio.com/meetups.json',
+            {
+                method: "POST",
+                body: JSON.stringify(meetupData),
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            }
+        ).then(() => {
+            history.replace('/');
+        });
+    }
     return (
         <section>
             <h1>New Meetup</h1>
