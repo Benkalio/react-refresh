f93f67fc6ae8a0224cf00bb2dc49767b4e4ac553
Tennyson Ben-kalio <officialtennysonbk@gmail.com>
2022-06-06 17:05:12 +0100

    fixing bugs

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -5,7 +5,26 @@ import MeetupList from "../components/meetups/MeetupList";
 function AllMeetupsPage() {
     const [isLoading, setIsLoading] = useState(true);
     const [loadedMeetups, setLoadedMeetups] = useState([]);
-
+    const DUMMY_DATA = [
+        {
+          id: 'm1',
+          title: 'This is a first meetup',
+          image:
+            'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
+          address: 'Meetupstreet 5, 12345 Meetup City',
+          description:
+            'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
+        },
+        {
+          id: 'm2',
+          title: 'This is a second meetup',
+          image:
+            'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
+          address: 'Meetupstreet 5, 12345 Meetup City',
+          description:
+            'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
+        },
+    ];
     useEffect(() => {
         setIsLoading(true);
         /* SETTING MEETUPS TO FIREBASE */
@@ -41,7 +60,7 @@ function AllMeetupsPage() {
     return (
         <section>
             <h1>All Meetups Locations</h1>
-            <MeetupList meetups={loadedMeetups} />
+            <MeetupList meetups={DUMMY_DATA} />
         </section>
     );
 }
